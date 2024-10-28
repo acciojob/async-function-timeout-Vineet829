bf7160e3258f4d088f4bc6530c6b7abfb4a3332e
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-10-28 12:51:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+const text = document.querySelector("#text");
+const delay = document.querySelector("#delay");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", async () => {
+    let data = text.value;
+    let delayTime = parseInt(delay.value, 10) || 0; 
+
+    let result = await new Promise(resolve => {
+        setTimeout(() => {
+            resolve(data);
+        }, delayTime);
+    });
+
+    document.body.innerHTML = `${result}`;
+});
